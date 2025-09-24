e31069c292fcf981fe9d2fbf27fecb8809ad7ea7
stuartfarmer1324 <stuartfarmer1324@gmail.com>
2025-09-24 14:24:35 +0100

    I added a comment

--- shakespearepenguin.R
+++ shakespearepenguin.R
@@ -3,10 +3,13 @@ pd <- read.table("C:\\Users\\stuar\\Documents\\palmer penguins\\DATA\\penguin_da
 pd
 model1 <- lm(bill_length_mm ~ bill_depth_mm, pd)
 
+#This is a comment
+
 plot_penguin <- ggplot(data = model1, 
                        aes(x=bill_length_mm, y=bill_depth_mm))+
   stat_summary(geom="pointrange", fun.data=mean_se)+
-  theme_classic()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme_classic()
 plot_penguin
+
+
 
